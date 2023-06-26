ce587915d255a9b358215b501440d5496f9c9312
mustafa <mustafamcm9@gmail.com>
2023-06-26 21:41:43 +0300

    add fetch file

---
+++ fetch.py
@@ -0,0 +1,10 @@
+import requests
+from bs4 import BeautifulSoup
+
+def getTextsFromUrl(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    paragraphs = soup.find_all('p')
+    text = ' '.join([p.get_text() for p in paragraphs])
+    return text
+
