6b35246ff5bd15c62ba3a337b37aa177b966e29f
mustafa <mustafamcm9@gmail.com>
2023-06-26 22:47:14 +0300

    sanitize func

---
+++ sanitize.py
@@ -0,0 +1,22 @@
+import re
+import html
+import unicodedata
+
+def sanitize_text(text):
+    # Remove HTML tags
+    text = re.sub(r"<.*?>", "", text)
+
+    # Handle special characters
+    text = html.unescape(text)
+
+    # Normalize text
+    text = unicodedata.normalize("NFKD", text).encode("ascii", "ignore").decode("utf-8")
+
+    # Remove noisy data
+    # For example, removing consecutive whitespace, extra newlines, or special characters
+    text = re.sub(r"\s+", " ", text)
+    text = re.sub(r"\n+", "\n", text)
+    text = re.sub(r"[^a-zA-Z0-9\n.,?! ]", "", text)
+
+
+    return text
