ac948642ee706e688010420fbd2d8e0d9232562e
mustafa <mustafamcm9@gmail.com>
2023-06-27 10:49:16 +0300

    rename file

--- detect-lang.py
+++
@@ -1,5 +0,0 @@
-from lingua import LanguageDetectorBuilder
-
-def detect_language_of_text(text):
-  detector = LanguageDetectorBuilder.from_all_languages().with_preloaded_language_models().build() 
-  return detector.detect_language_of(text)
