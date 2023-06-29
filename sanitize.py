22ee9c506cf5bd4f2bd68712092593fa47ecff23
mustafa <mustafamcm9@gmail.com>
2023-06-29 13:30:45 +0300

    remove special characters when sanitizing

--- sanitize.py
+++ sanitize.py
@@ -12,11 +12,13 @@ def sanitize_text(text):
     # Normalize text
     text = unicodedata.normalize("NFKD", text).encode("ascii", "ignore").decode("utf-8")
 
+    # Replace special characters
+    text = text.replace("ı", "i").replace("ğ", "g").replace("ö", "o").replace("ü", "u").replace("ç", "c")
+
     # Remove noisy data
     # For example, removing consecutive whitespace, extra newlines, or special characters
     text = re.sub(r"\s+", " ", text)
     text = re.sub(r"\n+", "\n", text)
     text = re.sub(r"[^a-zA-Z0-9\n.,?! ]", "", text)
-
 
     return text
