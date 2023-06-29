09f00fba9515ecec56b5d6c5999177a95a9676cc
mustafa <mustafamcm9@gmail.com>
2023-06-29 19:27:43 +0300

    decrease cache time

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from flask_caching import Cache
 config = {
     "DEBUG": True,          # some Flask specific configs
     "CACHE_TYPE": "SimpleCache",  # Flask-Caching related configs
-    "CACHE_DEFAULT_TIMEOUT": 50
+    "CACHE_DEFAULT_TIMEOUT": 20
 }
 app = Flask(__name__)
 # tell Flask to use the above defined config
