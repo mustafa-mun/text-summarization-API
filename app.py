525c4b61467d5b1b453bff271a4c6c12375e8c6f
mustafa <mustafamcm9@gmail.com>
2023-06-29 12:32:25 +0300

    add file summarization

--- app.py
+++ app.py
@@ -19,79 +19,112 @@ cache = Cache(app)
 @app.route("/healthz")
 def healthz():
     return "OK", 200
-
-@app.route("/summarizeExtractive", methods=["POST"])
+## EXTRACTIVE TEXT ##
+@app.route("/summarizeTextExtractive", methods=["POST"])
 @cache.cached()
 async def summarize_text_extractive():
     text_param = request.args.get("text")
-    url_param = request.args.get("url")
-    text = None
-    if not text_param and not url_param:
-        error_response = jsonify(error="Missing or empty 'text' or 'url' parameter")
-        return error_response, 400
-    if text_param and url_param:
-        error_response = jsonify(error="Choose between 'text' or 'url' parameter")
+
+    if not text_param:
+        error_response = jsonify(error="Missing or empty 'text' parameter")
         return error_response, 400
     
-    if text_param:
-        text = text_param
-    if url_param:
-        # Extract text from the URL
-        text = await get_texts_from_url(url_param)
+    resp = await extractive_handler(text_param)
+    return resp
+## ABSTRACTIVE TEXT ##
+@app.route("/summarizeTextAbstractive", methods=["POST"])
+@cache.cached()
+async def summarize_text_abstractive():
+    text_param = request.args.get("text")
+    if not text_param:
+        error_response = jsonify(error="Missing or empty 'text' parameter")
+        return error_response, 400
+    resp = await abstractive_handler(text_param)
+    return resp
+## EXTRACTIVE URL ##
+@app.route("/summarizeUrlExtractive", methods=["POST"])
+@cache.cached()
+async def summarize_url_extractive():
+    url_param = request.args.get("url")
+    if not url_param:
+        error_response = jsonify(error="Missing or empty 'url' parameter")
+        return error_response, 400
 
+     # Extract text from the URL
+    text = await get_texts_from_url(url_param)
+    resp = await extractive_handler(text)
+    return resp
+## ABSTRACTIVE URL ##
+@app.route("/summarizeUrlAbstractive", methods=["POST"])
+@cache.cached()
+async def summarize_url_abstractive():
+    url_param = request.args.get("url")
+    
+    if not url_param:
+        error_response = jsonify(error="Missing or empty 'url' parameter")
+        return error_response, 400
+
+     # Extract text from the URL
+    text = await get_texts_from_url(url_param)
+    resp = await abstractive_handler(text)
+    return resp
+## EXTRACTIVE FILE ##
+@app.route("/summarizeFileExtractive", methods=["POST"])
+@cache.cached()
+async def summarize_file_extractive():
+    file_param = request.args.get("file")
+    if not file_param:
+        error_response = jsonify(error="Missing or empty 'file' parameter")
+        return error_response, 400
+
+     # Extract text from the file
+    text = await read_file(file_param)
+    resp = await extractive_handler(text)
+    return resp
+## ABSTRACTIVE FILE ##
+@app.route("/summarizeFileAbstractive", methods=["POST"])
+@cache.cached()
+async def summarize_file_abstractive():
+    file_param = request.args.get("file")
+    
+    if not file_param:
+        error_response = jsonify(error="Missing or empty 'file' parameter")
+        return error_response, 400
+
+     # Extract text from the URL
+    text = await read_file(file_param)
+    resp = await abstractive_handler(text)
+    return resp
+
+async def extractive_handler(text):
     language = await detect_language_of_text(text)
     num_sentences = request.args.get("num_sentences")
-    
     
     if not num_sentences:
         num_sentences = 3 # default num sentences
 
     summary = await summarize_extractive(text, num_sentences, language)
 
-
     resp = jsonify(summarized_text = summary, text_language = language.name)
     resp.mimetype = 'application/json'
     return resp
-    
-@app.route("/summarizeAbstractive", methods=["POST"])
-@cache.cached()
-async def summarize_text_abstractive():
-    text_param = request.args.get("text")
-    url_param = request.args.get("url")
-    text = None
+
+async def abstractive_handler(text):
     min_length_param = request.args.get("min")
     max_length_param = request.args.get("max")
 
-    if not text_param and not url_param:
-        error_response = jsonify(error="Missing or empty 'text' or 'url' parameter")
-        return error_response, 400
-    if text_param and url_param:
-        error_response = jsonify(error="Choose between 'text' or 'url' parameter")
-        return error_response, 400
-    
-    if text_param:
-        text = text_param
-    if url_param:
-        # Extract text from the URL
-        text = await get_texts_from_url(url_param)
-
     language = await detect_language_of_text(text)
-    num_sentences = request.args.get("num_sentences")
-    
-    if not num_sentences:
-        num_sentences = 3 # default num sentences
 
-    if not min_length_param or min_length_param > 100: # min length can be maximum 100
+    if not min_length_param or int(min_length_param) > 100: # min length can be maximum 100
         min_length_param = 50 # default min length
-    if not max_length_param or max_length_param > 200: # max length can be maximum 200
+    if not max_length_param or int(max_length_param) > 200: # max length can be maximum 200
         max_length_param = 80 # default max length
 
-    summary = summarize_abstractive(text, min_length_param, max_length_param)
+    summary = await summarize_abstractive(text, int(min_length_param), int(max_length_param))
 
     resp = jsonify(summarized_text = summary, text_language = language.name)
     resp.mimetype = 'application/json'
     return resp
-    
 
 if __name__ == '__main__':
     app.run(debug=True)
--- fetch.py
+++ fetch.py
@@ -1,4 +1,5 @@
 import requests
+from sanitize import *
 from bs4 import BeautifulSoup
 
 async def get_texts_from_url(url):
@@ -8,3 +9,7 @@ async def get_texts_from_url(url):
     text = ' '.join([p.get_text() for p in paragraphs])
     return text
 
+async def read_file(path_to_file):
+    file = open(path_to_file, "r")
+    sanitized_file = sanitize_text(file.read())
+    return sanitized_file
