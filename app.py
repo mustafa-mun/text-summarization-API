087482326c5d0a348c4024af89c6e31870470941
mustafa <mustafamcm9@gmail.com>
2023-07-01 15:41:21 +0300

    mount app to the v1

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, Blueprint
 from controllers import summarizeExtractiveController, summarizeAbstractiveController, translateController
 from flask_caching import Cache
 
@@ -7,13 +7,23 @@ config = {
     "CACHE_TYPE": "SimpleCache",  # Flask-Caching related configs
     "CACHE_DEFAULT_TIMEOUT": 60
 }
+
 app = Flask(__name__)
-app.register_blueprint(summarizeExtractiveController.summarize_ext_blueprint)
-app.register_blueprint(summarizeAbstractiveController.summarize_abs_blueprint)
-app.register_blueprint(translateController.translate_blueprint)
+
+# Create a Blueprint for v1
+v1_blueprint = Blueprint('v1', __name__, url_prefix='/v1')
+
+# Register the blueprints to the v1 blueprint
+v1_blueprint.register_blueprint(summarizeExtractiveController.summarize_ext_blueprint)
+v1_blueprint.register_blueprint(summarizeAbstractiveController.summarize_abs_blueprint)
+v1_blueprint.register_blueprint(translateController.translate_blueprint)
+
+# Register the v1 blueprint to the app
+app.register_blueprint(v1_blueprint)
 # tell Flask to use the above defined config
 app.config.from_mapping(config)
 cache = Cache(app)
+
 summarizeExtractiveController.cache.init_app(app, config=config)
 summarizeAbstractiveController.cache.init_app(app, config=config)
 translateController.cache.init_app(app, config=config)
@@ -22,3 +32,6 @@ translateController.cache.init_app(app, config=config)
 @app.route("/healthz")
 def healthz():
     return "OK", 200
+
+if __name__ == "__main__":
+    app.run(port=5000)
