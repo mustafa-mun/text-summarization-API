1eb299f182a43fb225225c4735df246ec752f3fd
mustafa <mustafamcm9@gmail.com>
2023-07-03 02:13:46 +0300

    remove v1 prefix   Reason: Deployment issues

--- app.py
+++ app.py
@@ -3,17 +3,6 @@ from controllers import summarizeExtractiveController, summarizeAbstractiveContr
 
 app = Flask(__name__)
 
-# Create a Blueprint for v1
-v1_blueprint = Blueprint('v1', __name__, url_prefix='/v1')
-
-# Register the blueprints to the v1 blueprint
-v1_blueprint.register_blueprint(summarizeExtractiveController.summarize_ext_blueprint)
-v1_blueprint.register_blueprint(summarizeAbstractiveController.summarize_abs_blueprint)
-v1_blueprint.register_blueprint(translateController.translate_blueprint)
-
-# Register the v1 blueprint to the app
-app.register_blueprint(v1_blueprint)
-
 @app.route("/")
 def index():
     resp, status = healthz()
